ba48418264543ca4770304faf8a2868b6d4fc5c4
BrandPilot <thasleenanasreen242@gmail.com>
2026-08-17 15:35:00 +0530

    feat: restore homepage WhatsApp entry point

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import LaelPage from "@/components/LaelPage";
 import LaelFooter from "@/components/LaelFooter";
+import { FloatingWhatsAppButton } from "@/components/FloatingWhatsAppButton";
 
 export default function Home() {
   return (
     <>
       <LaelPage />
       <LaelFooter />
+      <FloatingWhatsAppButton />
     </>
   );
 }
